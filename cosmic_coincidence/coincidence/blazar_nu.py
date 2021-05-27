253ff081dee594eeade16ef72f2bb08665d34359
Francesca Capel <capel.francesca@gmail.com>
2021-05-27 18:35:16 +0200

    Merge blazar nu sims with common base class to handle diffuse and connected

--- cosmic_coincidence/coincidence/blazar_nu.py
+++ cosmic_coincidence/coincidence/blazar_nu.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy as np
+from abc import ABCMeta, abstractmethod
 from joblib import (
     register_parallel_backend,
     parallel_backend,
@@ -24,6 +25,7 @@ from cosmic_coincidence.populations.aux_samplers import (
 )
 from cosmic_coincidence.populations.selection import GalacticPlaneSelection
 from cosmic_coincidence.neutrinos.icecube import (
+    IceCubeObsParams,
     IceCubeAlertsParams,
     IceCubeAlertsWrapper,
 )
@@ -32,6 +34,250 @@ from cosmic_coincidence.utils.package_data import get_path_to_data
 from cosmic_coincidence.utils.parallel import FileWritingBackend
 
 register_parallel_backend("file_write", FileWritingBackend)
+
+
+class BlazarNuSim(Simulation, metaclass=ABCMeta):
+    """
+    Abstract base class for blazar
+    neutrino simulations.
+    """
+
+    def __init__(
+        self,
+        file_name="output/test_sim.h5",
+        group_base_name="survey",
+        N=1,
+        bllac_config: str = None,
+        fsrq_config: str = None,
+        nu_config: str = None,
+        nu_hese_config: str = None,
+        nu_ehe_config: str = None,
+    ):
+
+        super().__init__(
+            file_name=file_name,
+            group_base_name=group_base_name,
+            N=N,
+        )
+
+        self._bllac_config = bllac_config
+        self._fsrq_config = fsrq_config
+        self._nu_config = nu_config
+        self._nu_hese_config = nu_hese_config
+        self._nu_ehe_config = nu_ehe_config
+
+        self._bllac_param_servers = []
+        self._fsrq_param_servers = []
+        self._nu_param_servers = []
+
+    def _setup_param_servers(self):
+
+        self._bllac_param_servers = []
+        self._fsrq_param_servers = []
+        self._nu_param_servers = []
+
+        for i in range(self._N):
+
+            seed = i * 100
+
+            # BL Lacs
+            bllac_spec = get_path_to_data(self._bllac_config)
+            bllac_param_server = PopsynthParams(bllac_spec)
+            bllac_param_server.seed = seed
+            bllac_param_server.file_name = self._file_name
+            bllac_param_server.group_name = self._group_base_name + "_%i" % i
+
+            self._bllac_param_servers.append(bllac_param_server)
+
+            # FSRQs
+            fsrq_spec = get_path_to_data(self._fsrq_config)
+            fsrq_param_server = PopsynthParams(fsrq_spec)
+            fsrq_param_server.seed = seed
+            fsrq_param_server.file_name = self._file_name
+            fsrq_param_server.group_name = self._group_base_name + "_%i" % i
+
+            self._fsrq_param_servers.append(fsrq_param_server)
+
+            # Neutrinos
+            if self._nu_config is not None:
+
+                nu_spec = get_path_to_data(self._nu_config)
+                nu_param_server = IceCubeObsParams(nu_spec)
+
+            else:
+
+                nu_hese_spec = get_path_to_data(self._nu_hese_config)
+                nu_ehe_spec = get_path_to_data(self._nu_ehe_config)
+                nu_param_server = IceCubeAlertsParams(nu_hese_spec, nu_ehe_spec)
+
+            nu_param_server.seed = seed
+            nu_param_server.file_name = self._file_name
+            nu_param_server.group_name = self._group_base_name + "_%i" % i
+
+            self._nu_param_servers.append(nu_param_server)
+
+    @abstractmethod
+    def _bllac_pop_wrapper(self, param_server):
+
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _fsrq_pop_wrapper(self, param_server):
+
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _nu_pop_wrapper(self, param_server):
+
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _blazar_nu_wrapper(self, bllac_pop, fsrq_pop, nu_obs):
+
+        raise NotImplementedError()
+
+    def _sim_wrapper(self, bllac_param_server, fsrq_param_server, nu_param_server):
+
+        bllac_pop = self._bllac_pop_wrapper(bllac_param_server)
+
+        fsrq_pop = self._fsrq_pop_wrapper(fsrq_param_server)
+
+        nu_obs = self._nu_obs_wrapper(nu_param_server)
+
+        result = self._blazar_nu_wrapper(bllac_pop, fsrq_pop, nu_obs)
+
+        del bllac_pop, fsrq_pop, nu_obs
+
+        return result
+
+    def run(self, parallel=True, n_jobs=4):
+
+        # Parallel
+        if parallel:
+
+            # Writes to file upon completion
+            with parallel_backend("file_write"):
+
+                Parallel(n_jobs=n_jobs)(
+                    delayed(self._sim_wrapper)(bllac_ps, fsrq_ps, nu_ps)
+                    for bllac_ps, fsrq_ps, nu_ps in zip(
+                        self._bllac_param_servers,
+                        self._fsrq_param_servers,
+                        self._nu_param_servers,
+                    )
+                )
+
+        # Serial
+        else:
+
+            for bllac_ps, fsrq_ps, nu_ps in zip(
+                self._bllac_param_servers,
+                self._fsrq_param_servers,
+                self._nu_param_servers,
+            ):
+
+                result = self._sim_wrapper(
+                    bllac_ps,
+                    fsrq_ps,
+                    nu_ps,
+                )
+
+                result.write()
+
+                del result
+
+
+class BlazarNuCoincidenceSim(BlazarNuSim):
+    """
+    Set up and run simulations for blazar-neutrino
+    coincidences. Assumes blazars and neutrinos have
+    no underlying connection.
+    """
+
+    def __init__(
+        self,
+        file_name="output/test_coincidence_sim.h5",
+        group_base_name="survey",
+        N=1,
+        bllac_config: str = None,
+        fsrq_config: str = None,
+        nu_config: str = None,
+        nu_hese_config: str = None,
+        nu_ehe_config: str = None,
+    ):
+
+        super().__init__(
+            file_name=file_name,
+            group_base_name=group_base_name,
+            N=N,
+            bllac_config=bllac_config,
+            fsrq_config=fsrq_config,
+            nu_config=nu_config,
+            nu_hese_config=nu_hese_config,
+            nu_ehe_config=nu_ehe_config,
+        )
+
+    def _bllac_pop_wrapper(self, param_server):
+
+        return PopsynthWrapper(param_server)
+
+    def _fsrq_pop_wrapper(self, param_server):
+
+        return PopsynthWrapper(param_server)
+
+    def _nu_obs_wrapper(self, param_server):
+
+        return IceCubeAlertsWrapper(param_server)
+
+    def _blazar_nu_wrapper(self, bllac_pop, fsrq_pop, nu_obs):
+
+        return BlazarNuCoincidence(bllac_pop, fsrq_pop, nu_obs)
+
+
+class BlazarNuConnectedSim(BlazarNuSim):
+    """
+    Set up and run simulations of neutrinos produced
+    by blazars.
+    """
+
+    def __init__(
+        self,
+        file_name="output/test_connected_sim.h5",
+        group_base_name="survey",
+        N=1,
+        bllac_config: str = None,
+        fsrq_config: str = None,
+        nu_config: str = None,
+        nu_hese_config: str = None,
+        nu_ehe_config: str = None,
+    ):
+
+        super().__init__(
+            file_name=file_name,
+            group_base_name=group_base_name,
+            N=N,
+            bllac_config=bllac_config,
+            fsrq_config=fsrq_config,
+            nu_config=nu_config,
+            nu_hese_config=nu_hese_config,
+            nu_ehe_config=nu_ehe_config,
+        )
+
+    def _bllac_pop_wrapper(self, param_server):
+
+        return PopsynthWrapper(param_server)
+
+    def _fsrq_pop_wrapper(self, param_server):
+
+        return PopsynthWrapper(param_server)
+
+    def _nu_obs_wrapper(self, param_server):
+
+        return IceCubeAlertsWrapper(param_server)
+
+    def _blazar_nu_wrapper(self, nu_obs, bllac_pop, fsrq_pop):
+
+        return BlazarNuConnection(nu_obs, bllac_pop, fsrq_pop)
 
 
 class BlazarNuCoincidence(object):
@@ -192,200 +438,12 @@ class BlazarNuCoincidence(object):
         self.fsrq_coincidence["n_flaring"] = n_match_flaring
 
 
-class BlazarNuCoincidenceSim(Simulation):
+class BlazarNuConnection(object):
     """
-    Set up and run simulations for blazar-neutrino
-    coincidences. Assumes blazars and neutrinos have
-    no underlying connection.
-    """
-
-    def __init__(
-        self,
-        file_name="output/test_coincidence_sim.h5",
-        group_base_name="survey",
-        N=1,
-    ):
-
-        super().__init__(
-            file_name=file_name,
-            group_base_name=group_base_name,
-            N=N,
-        )
-
-    def _setup_param_servers(self):
-
-        self._bllac_param_servers = []
-        self._fsrq_param_servers = []
-        self._nu_param_servers = []
-
-        for i in range(self._N):
-
-            seed = i * 100
-
-            # BL Lacs
-            bllac_spec = get_path_to_data("bllac.yml")
-            bllac_param_server = PopsynthParams(bllac_spec)
-            bllac_param_server.seed = seed
-            bllac_param_server.file_name = self._file_name
-            bllac_param_server.group_name = self._group_base_name + "_%i" % i
-
-            self._bllac_param_servers.append(bllac_param_server)
-
-            # FSRQs
-            fsrq_spec = get_path_to_data("fsrq.yml")
-            fsrq_param_server = PopsynthParams(fsrq_spec)
-            fsrq_param_server.seed = seed
-            fsrq_param_server.file_name = self._file_name
-            fsrq_param_server.group_name = self._group_base_name + "_%i" % i
-
-            self._fsrq_param_servers.append(fsrq_param_server)
-
-            # Neutrinos
-            nu_hese_spec = get_path_to_data("diffuse_hese_nu.yml")
-            nu_ehe_spec = get_path_to_data("diffuse_ehe_nu.yml")
-            nu_param_server = IceCubeAlertsParams(nu_hese_spec, nu_ehe_spec)
-            nu_param_server.seed = seed
-            nu_param_server.file_name = self._file_name
-            nu_param_server.group_name = self._group_base_name + "_%i" % i
-
-            self._nu_param_servers.append(nu_param_server)
-
-    def _bllac_pop_wrapper(self, param_server):
-
-        return PopsynthWrapper(param_server)
-
-    def _fsrq_pop_wrapper(self, param_server):
-
-        return PopsynthWrapper(param_server)
-
-    def _nu_obs_wrapper(self, param_server):
-
-        return IceCubeAlertsWrapper(param_server)
-
-    def _sim_wrapper(self, bllac_param_server, fsrq_param_server, nu_param_server):
-
-        bllac_pop = self._bllac_pop_wrapper(bllac_param_server)
-
-        fsrq_pop = self._fsrq_pop_wrapper(fsrq_param_server)
-
-        nu_obs = self._nu_obs_wrapper(nu_param_server)
-
-        result = self._coincidence_check(bllac_pop, fsrq_pop, nu_obs)
-
-        del bllac_pop, fsrq_pop, nu_obs
-
-        return result
-
-    def _coincidence_check(self, bllac_pop, fsrq_pop, nu_obs):
-
-        return BlazarNuCoincidence(bllac_pop, fsrq_pop, nu_obs)
-
-    def run(self, parallel=True, n_jobs=4):
-
-        # Parallel
-        if parallel:
-
-            # Writes to file upon completion
-            with parallel_backend("file_write"):
-
-                Parallel(n_jobs=n_jobs)(
-                    delayed(self._sim_wrapper)(bllac_ps, fsrq_ps, nu_ps)
-                    for bllac_ps, fsrq_ps, nu_ps in zip(
-                        self._bllac_param_servers,
-                        self._fsrq_param_servers,
-                        self._nu_param_servers,
-                    )
-                )
-
-        # Serial
-        else:
-
-            for bllac_ps, fsrq_ps, nu_ps in zip(
-                self._bllac_param_servers,
-                self._fsrq_param_servers,
-                self._nu_param_servers,
-            ):
-
-                result = self._sim_wrapper(
-                    bllac_ps,
-                    fsrq_ps,
-                    nu_ps,
-                )
-
-                result.write()
-
-                del result
-
-
-def BlazarNuConnectedSim(Simulation):
-    """
-    Set up and run simulations of neutrinos produced
-    by blazars.
+    Handle connected blazar and neutrino
+    observations.
     """
 
-    def __init__(
-        self,
-        file_name="output/test_connected_sim.h5",
-        group_base_name="survey",
-        N=1,
-    ):
+    def __init__(self):
 
-        super().__init__(
-            file_name=file_name,
-            group_base_name=group_base_name,
-            N=N,
-        )
-
-    def _setup_param_servers(self):
-
-        self._bllac_param_servers = []
-        self._fsrq_param_servers = []
-        self._nu_param_servers = []
-
-        for i in range(self._N):
-
-            seed = i * 100
-
-            # BL Lacs
-            bllac_spec = get_path_to_data("bllac.yml")
-            bllac_param_server = PopsynthParams(bllac_spec)
-            bllac_param_server.seed = seed
-            bllac_param_server.file_name = self._file_name
-            bllac_param_server.group_name = self._group_base_name + "_%i" % i
-
-            self._bllac_param_servers.append(bllac_param_server)
-
-            # FSRQs
-            fsrq_spec = get_path_to_data("fsrq.yml")
-            fsrq_param_server = PopsynthParams(fsrq_spec)
-            fsrq_param_server.seed = seed
-            fsrq_param_server.file_name = self._file_name
-            fsrq_param_server.group_name = self._group_base_name + "_%i" % i
-
-            self._fsrq_param_servers.append(fsrq_param_server)
-
-            # Neutrinos
-            nu_hese_spec = get_path_to_data("connected_hese_nu.yml")
-            nu_ehe_spec = get_path_to_data("connected_ehe_nu.yml")
-            nu_param_server = IceCubeAlertsParams(nu_hese_spec, nu_ehe_spec)
-            nu_param_server.seed = seed
-            nu_param_server.file_name = self._file_name
-            nu_param_server.group_name = self._group_base_name + "_%i" % i
-
-            self._nu_param_servers.append(nu_param_server)
-
-    def _bllac_pop_wrapper(self, param_server):
-
-        return PopsynthWrapper(param_server)
-
-    def _fsrq_pop_wrapper(self, param_server):
-
-        return PopsynthWrapper(param_server)
-
-    def _nu_obs_wrapper(self, param_server):
-
-        return IceCubeAlertsWrapper(param_server)
-
-    def _connected_nu_wrapper(self, nu_obs, bllac_pop, fsrq_pop):
-
-        return BlazarNuConnection(nu_obs, bllac_pop, fsrq_pop)
+        raise NotImplementedError()
